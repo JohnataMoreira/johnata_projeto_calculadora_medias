52a457997d11f6cc6fcc4bd433aaf3c8594f68c5
JohnataMoreira <johnatavinicius@hotmail.com>
2024-04-28 20:54:17 -0300

    limitação de casas decimais

--- main.js
+++ main.js
@@ -24,19 +24,17 @@ function adicionaLinha(){
     if(atividades.includes(inputNomeAtividade.value)){
         alert(`A atividade: (${inputNomeAtividade.value}) já foi inserida.`);
     } else{
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
 
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += `</tr>`;
+
+        linhas += linha;
     }
-
-    atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
-
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
-    linha += `</tr>`;
-
-    linhas += linha;
 
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
@@ -50,8 +48,8 @@ function atualizaTabela(){
 function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal();
 
-    document.querySelector('media-final-valor').innerHTML = mediaFinal;
-    document.querySelector('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado:spanReprovado;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(1);
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado:spanReprovado;
 }
 
 function calculaMediaFinal(){
